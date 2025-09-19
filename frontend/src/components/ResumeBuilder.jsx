5e6d01375b08464f3e45efe4530fbe6695e0a857
snipverse <sachinpal9324@gmail.com>
2025-09-20 01:08:50 +0530

    fixed ResumeBuilder

--- frontend/src/components/ResumeBuilder.jsx
+++ frontend/src/components/ResumeBuilder.jsx
@@ -1,6 +1,6 @@
 import { useState } from 'react';
 import PropTypes from 'prop-types';
-import Navbar from '../shared/Navbar';
+import Navbar from './shared/Navbar';
 import { Button } from '../ui/button';
 import { Input } from '../ui/input';
 import { Label } from '../ui/label';
