02c5cfbe1d13c396025e36d31c6758a84e4602b6
snipverse <sachinpal9324@gmail.com>
2025-09-20 11:59:03 +0530

    added redio button for admin

--- frontend/src/components/auth/Login.jsx
+++ frontend/src/components/auth/Login.jsx
@@ -102,6 +102,17 @@ const Login = () => {
                                 />
                                 <Label htmlFor="r2">Recruiter</Label>
                             </div>
+                            <div className="flex items-center space-x-2">
+                                <Input
+                                    type="radio"
+                                    name="role"
+                                    value="admin"
+                                    checked={input.role === 'admin'}
+                                    onChange={changeEventHandler}
+                                    className="cursor-pointer"
+                                />
+                                <Label htmlFor="r3">Admin</Label>
+                            </div>
                         </RadioGroup>
                     </div>
                     {
