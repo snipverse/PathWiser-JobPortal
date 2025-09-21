ace7ff02af519a49af36c3a772a1c93dc35c61d7
snipverse <sachinpal9324@gmail.com>
2025-09-21 23:45:47 +0530

    CORS Fixed to allow PATCH request

--- backend/index.js
+++ backend/index.js
@@ -31,7 +31,7 @@ app.use((req, res, next) => {
   if (allowedOrigins.includes(origin)) {
     res.header('Access-Control-Allow-Origin', origin);
     res.header('Access-Control-Allow-Credentials', 'true');
-    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,PATCH,OPTIONS');
     res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
   }
   if (req.method === 'OPTIONS') {
